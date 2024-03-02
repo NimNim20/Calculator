7374974479a627c0c8ad0dcefada2e79a06fb89a
Benjamin Vid Gard <benvio01@easv365.dk>
2024-03-03 00:33:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function clearDisplay(){
 
 function calculate(){
   try{
-    display.value = eval(display.value);
+    display.value = eval(display.value); //This line here is called "dangerous code" because if it gives an error, it will not display it properly, so we use the try/catch block to try the function and catch any errors.
   }
   catch(error){
     display.value = "Error";
